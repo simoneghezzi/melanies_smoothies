f79e517be16cb555dfd19ae256d144f8f58a0509
Simone Ghezzi <simone.ghezzi@gmail.com>
2025-07-19 19:57:40 +0000

    added requests library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
+import requests
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize your Smoothie! :cup_with_straw:")
