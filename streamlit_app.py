d84cd0f5b526989e40c5f87a6a2d54eb9b170a69
Simone Ghezzi <simone.ghezzi@gmail.com>
2025-07-19 20:20:18 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-st.text(smoothiefroot_response.json())        
-#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+#st.text(smoothiefroot_response.json())        
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
