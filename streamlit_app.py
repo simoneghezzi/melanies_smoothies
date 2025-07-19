2e021be436c00f17ff4db08f95fc51d759431044
Simone Ghezzi <simone.ghezzi@gmail.com>
2025-07-19 20:01:31 +0000

    requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
 
-        
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)        
