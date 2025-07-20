aa266c434a64d365bbc4c2003f64117832dd2535
Simone Ghezzi <simone.ghezzi@gmail.com>
2025-07-20 05:59:27 +0000

    detail for each ingredient

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")      
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
@@ -44,9 +48,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
 
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
-#st.text(smoothiefroot_response.json())        
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
